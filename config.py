c0ec1c6ab4b9a6f6229448bd49ecf2fa98c75be0
Telegram Crypto Groups <40732656+coinalexo@users.noreply.github.com>
2026-03-01 17:15:08 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-BOT_TOKEN = "8675246572:AAGeZts7LbSZ1Q8tvRrhl0QaLf8-OJgYXUM"
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 RPC_URL = "https://api.mainnet-beta.solana.com"
-
-# твой быстрый RPC можно вставить сюда:
-# RPC_URL = "https://mainnet.helius-rpc.com/?api-key=XXXX"
-
 JUPITER_SLIPPAGE_BPS = 1500
-
