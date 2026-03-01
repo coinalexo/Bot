18582ace78ea246f5cb5ba65672dfd9348259a00
Telegram Crypto Groups <40732656+coinalexo@users.noreply.github.com>
2026-03-01 17:28:25 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import router
 from config import BOT_TOKEN
+from db import init_db   # ✅ ДОБАВИТЬ
 
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
+
+init_db()   # ✅ создаёт таблицы
 
 dp.include_router(router)
 
